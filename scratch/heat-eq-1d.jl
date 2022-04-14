32de8935bfb544d4e33092a1078f5c211bc6b622
satyam <satyam@technoculture.io>
2022-04-14 18:20:22 +0530

    theme for plots, using vs code now

--- scratch/heat-eq-1d.jl
+++ scratch/heat-eq-1d.jl
@@ -1,14 +1,31 @@
-using Pkg; Pkg.add(["DifferentialEquations", "Plots"])
+using Pkg;
+Pkg.add(["DifferentialEquations", "Plots", "PlotThemes", "PlotlyJS", "Latexify"]);
 
 using DifferentialEquations
 using Plots
+plotlyjs()
+theme(:vibrant)
+# theme(:dracula)
+
+using Latexify
 
 Δt = 0.01
 Δx = 0.01
 
 t = 0:Δt:1
-x = 0:Δx:1
+x = 0:Δx:5
 
-u = (x, t) -> 15*exp(sin(2π*t) + cos(π*t)) + x
+func = :(u(x, t) = 15 * exp(sin(2π * t)))
+eval(func)
 
-surface(t, x, u, xaxis = "t [sec]", yaxis = "x [m]", zaxis = "T [°C]")
+latexify(func) |> render
+
+# surface(t, x, u, xaxis="t [sec]", yaxis="x [m]", zaxis="T [°C]")
+contour(
+  t, x, u,
+  levels=10,
+  fill=true,
+  xaxis="t [sec]",
+  yaxis="x [m]",
+  zaxis="T [°C]"
+)
